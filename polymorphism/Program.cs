f45ae629520f934a7685cf74ebb134aad87d8d0e
Jamie Myland <jamie.myland@gmail.com>
2020-10-22 10:00:08 +0100

    Created double addition method

--- polymorphism/Program.cs
+++ polymorphism/Program.cs
@@ -7,11 +7,18 @@ namespace polymorphism
         static void Main(string[] args)
         {
             int addedSumOfInts = AddTwoInts(7, 13);
+            double addedSumOfDoubles = AddTwoDoubles(7.5, 13.8);
 
             Console.WriteLine("Int: " + addedSumOfInts);
+            Console.WriteLine("Double: " + addedSumOfDoubles);
         }
 
         static int AddTwoInts(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double AddTwoDoubles(double x, double y)
         {
             return x + y;
         }
